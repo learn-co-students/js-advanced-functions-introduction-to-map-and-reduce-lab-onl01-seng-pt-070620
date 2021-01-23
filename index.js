451f6697e64167e0fee950232a8ac98932745d74
khargr14 <kay_hargrove@yahoo.com>
2021-01-23 17:41:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+const mapToNegativize = function(arr) {
+    let toReturn = [];
+    for( let element of arr ) {
+      toReturn.push(element * -1);
+    }
+    return toReturn;
+  }
+  const mapToNoChange = function(arr) {
+    let toReturn = [];
+    for(let element of arr ) {
+      toReturn.push(element)
+    }
+    return toReturn;
+  }
+  const mapToDouble = function(arr) {
+    let toReturn = [];
+    for( let element of arr ) {
+      toReturn.push(element * 2);
+    }
+    return toReturn;
+  }
+  const mapToSquare = function(arr) {
+    let toReturn = [];
+    for( let element of arr ) {
+      toReturn.push(element ** 2);
+    }
+    return toReturn;
+  }
+  const reduceToTotal = function(arr, base=0){
+    for( let element of arr ){
+      base += element;
+    }
+    return base;
+  }
+  const reduceToAllTrue = function(arr) {
+    let returnValue = true;
+    for( let element of arr ){
+      if(!element) {
+        returnValue = false;
+      }
+    }
+    return returnValue;
+  }
+  const reduceToAnyTrue = function(arr) {
+    let returnValue = false;
+    for( let element of arr ){
+      if(element){
+        returnValue = true;
+      }
+    }
+    return returnValue;
+  }
